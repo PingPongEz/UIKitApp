1e3d9d7f73ffffcafb3d87354c8f959ad03113d5
Sergey <kopgamerk@gmail.com>
2022-03-14 13:51:05 +0300

    SomeChanges

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var testLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var sliderForLabel: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testLabel.font = testLabel.font.withSize(24)
+        testLabel.textColor = .orange
+        testLabel.textAlignment = .natural
+        testLabel.numberOfLines = 3
+        
+        segmentedControl.selectedSegmentIndex = -1
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        segmentedControl.setTitle("First", forSegmentAt: 0)
+        segmentedControl.setTitle("Second", forSegmentAt: 1)
+        
+        sliderForLabel.minimumValue = 12
+        sliderForLabel.maximumValue = 24
+        sliderForLabel.value = sliderForLabel.maximumValue
+        testLabel.font = testLabel.font.withSize(CGFloat(sliderForLabel.value))
+        
     }
 
-
+    
+    @IBAction func segmentedControlSwitch() {
+        switch segmentedControlSwitch {
+        case 0: testLabel.text = "First one selected"
+        case 1: testLabel.text = "Second one selected"
+        default: testLabel.text = "Third one selected"
+        }
+    }
+    
+    @IBAction func sliderDoingSomething() {
+        testLabel.font = testLabel.font.withSize(CGFloat(sliderForLabel.value))
+    }
+    
 }
 
